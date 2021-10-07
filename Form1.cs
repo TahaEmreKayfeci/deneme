87fc4fcbf68b4436b4846aa8d68e59ce2c12c421
Taha Emre Kayfeci <taha.kayfeci@mrevyap.k12.tr>
2021-10-07 08:13:57 +0300

    2. Gün

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace deneme
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Deneme'nin 2. Gün'ü");
+        }
     }
 }
